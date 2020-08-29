1f2663f024d72be80850f53ec4acb06c00876b09
DhanushkumarSivaji <dhanushkumar.s@yahoo.com>
2020-08-29 20:35:15 +0530

    Completed pagination service and added context-path

--- src/main/java/com/dhanush/app/ws/controller/UserController.java
+++ src/main/java/com/dhanush/app/ws/controller/UserController.java
@@ -2,6 +2,9 @@ package com.dhanush.app.ws.controller;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dhanush.app.ws.exceptions.UserServiceException;
@@ -68,7 +72,7 @@ public class UserController {
 		return returnValue;
 	};
 	
-	@DeleteMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	@DeleteMapping(path = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public OperationStatusModel deleteUser(@PathVariable String id) {
 		OperationStatusModel returnValue = new OperationStatusModel();
 		returnValue.setOperationName(RequestOperationName.DELETE.name());
@@ -78,5 +82,21 @@ public class UserController {
 		returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
 		return returnValue;
 	};
+	
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public List<UserResponse> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "5") int limit) {
+		List<UserResponse> returnValue = new ArrayList<>();
+
+		List<UserDto> users = userService.getUsers(page, limit);
+		
+		for (UserDto userDto : users) {
+			UserResponse userModel = new UserResponse();
+			BeanUtils.copyProperties(userDto, userModel);
+			returnValue.add(userModel);
+		}
+
+		return returnValue;
+	}
 
 }
--- src/main/java/com/dhanush/app/ws/repository/UserRepository.java
+++ src/main/java/com/dhanush/app/ws/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.dhanush.app.ws.repository;
 
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dhanush.app.ws.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
 	UserEntity findByUserId(String userId);
 }
